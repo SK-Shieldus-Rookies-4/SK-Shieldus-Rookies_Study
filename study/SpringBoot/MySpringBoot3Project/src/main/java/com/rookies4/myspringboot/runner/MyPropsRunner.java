e339e46fc3632bed42ac6b8f70e8b9ace24c5bf7
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-08-12 16:08:33 +0900

    ApplicationArguments

---
+++ study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
@@ -0,0 +1,19 @@
+package com.rookies4.myspringboot.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyPropsRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Vm Argments " + args.containsOption("foo"));
+        System.out.println("Program Arguments = " + args.containsOption("bar"));
+
+        for(String argName: args.getOptionNames()) {
+            System.out.println("argName = " + argName);
+        }
+    }
+}
