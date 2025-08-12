eba2e8af1b941be25409686095090fe897b07265
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-08-12 17:07:27 +0900

    람다

--- study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
+++ study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
@@ -4,16 +4,32 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Consumer;
+
 @Component
 public class MyPropsRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("Vm Argments " + args.containsOption("foo"));
-        System.out.println("Program Arguments = " + args.containsOption("bar"));
+        System.out.println("Vm Argments " + args.containsOption("foo")); //false
+        System.out.println("Program Arguments = " + args.containsOption("bar")); //true
 
         for(String argName: args.getOptionNames()) {
             System.out.println("아규먼트 이름 = " + argName);
         }
+
+        //args.containsOptionNames()의 리턴타입 Set<String>
+        //Interable의 foreach(Consumer) 메서드 호출하기
+        //Consumer의 추상메서드 void accept(T t)
+        //1. 익명의 Inner class (Anonymous Inner Class)
+        args.getOptionNames().forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("Inner Class 아규먼트 이름 = " + s);
+            }
+        });
+
+        //2. 함수형 인터페이스 (람다함수)
+        args.getOptionNames().forEach(name -> System.out.println(name));
     }
 }
