c921092d7a66de201e485601186aded2b949ed70
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-08-13 19:28:16 +0900

    jpa entity update

--- study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/entity/Customer.java
+++ study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/entity/Customer.java
@@ -3,14 +3,17 @@ package com.rookies4.myspringboot.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "customers")
 @Getter @Setter
 public class Customer {
     //Primary Key, PK값을 Persistence Provider가 결정해라
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     //Unique한 값을 가져야 하고, Null값을 허용하지 않는다.
@@ -20,5 +23,9 @@ public class Customer {
     //Null값을 허용하지 않는다.
     @Column(nullable = false)
     private String customerName;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    LocalDateTime createdAt = LocalDateTime.now();
 
 }
