eefcc3c3f6dcd38e34eb0d8597d2612387769468
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-08-13 10:54:49 +0900

    ProdConfig add1

--- study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/config/ProdConfig.java
+++ study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/config/ProdConfig.java
@@ -8,7 +8,10 @@ import org.springframework.context.annotation.Configuration;
 public class ProdConfig {
     @Bean
     public CustomVO customVO() {
-
+        return CustomVO.builder() //CustomerVOBuilder
+                .mode("운영모드")
+                .rate(1.5)
+                .build(); //CustomerVO
     }
 
 }
