12f08c5337d9c042547a244210eb5890149c774a
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-08-14 11:12:25 +0900

    CustomerRepositoryTest update

--- study/SpringBoot/MySpringBoot3Project/src/test/java/com/rookies4/myspringboot/repository/CustomerRepositoryTest.java
+++ study/SpringBoot/MySpringBoot3Project/src/test/java/com/rookies4/myspringboot/repository/CustomerRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.rookies4.myspringboot.repository;
 
 import com.rookies4.myspringboot.entity.Customer;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 //assertj 라이브러리의 Assertions 클래스
@@ -16,18 +19,30 @@ class CustomerRepositoryTest {
     CustomerRepository customerRepository;
 
     @Test
+    //Customer 조회
+    void testFindCustomer() {
+        //findById() 호출
+        Optional<Customer> customerById = customerRepository.findById(1L);
+
+    }
+
+    @Test
     @Rollback(value = false) //Rollback 처리하지 마세요!!
+    @Disabled
+    //Customer 등록
     void testSaveCustomer() {
         //Given(준비단계)
         Customer customer = new Customer();
-        customer.setCustomerId("AC001");
-        customer.setCustomerName("스프링부트");
+        customer.setCustomerId("AC002");
+        customer.setCustomerName("스프링FW");
         //When(실행단계)
         Customer savedCustomer = customerRepository.save(customer);
         //Then(검증단계)
         //assertEquals(expected,actual) - 이전 프레임워크에서
         //등록된 Customer 엔티티객체가 Null이 아닌지를 검증하기
         assertThat(savedCustomer).isNotNull();
+        //등록된 Customer Name값이 동일한지 검증하기
+        assertThat(savedCustomer.getCustomerName()).isEqualTo("스프링FW");
 
     }
 
