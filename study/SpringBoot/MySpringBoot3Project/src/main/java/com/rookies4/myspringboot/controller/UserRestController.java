aca0ebace4c6165f62569ce178c7bd6b38ac4449
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-08-14 16:55:35 +0900

    UserRestController getUsers

--- study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
+++ study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
@@ -1,11 +1,15 @@
 package com.rookies4.myspringboot.controller;
 
+import com.rookies4.myspringboot.entity.UserEntity;
 import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor // final이 붙은 변수의 생성자를 초기화해줌
+@RequestMapping("/api/users")
 public class UserRestController {
     private final UserRepository userRepository; // final 붙이면 선언과 동시에 초기화 필요
 
@@ -15,5 +19,14 @@ public class UserRestController {
 //        this.userRepository = userRepository;
 //    }
 
+    @PostMapping //등록
+    public UserEntity create(@RequestBody UserEntity user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<UserEntity> getUsers() {
+        return userRepository.findAll();
+    }
 
 }
--- study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/entity/UserEntity.java
+++ study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/entity/UserEntity.java
@@ -18,8 +18,10 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String name;
+
     @Column(unique = true, nullable = false)
     private String email;
 
