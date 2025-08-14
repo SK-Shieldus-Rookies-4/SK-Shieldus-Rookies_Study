b6ec83a778fbd7a8da6ab74dd7d66f5da147824a
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-08-14 17:18:44 +0900

    UserEntity getUser

--- study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
+++ study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
@@ -1,11 +1,14 @@
 package com.rookies4.myspringboot.controller;
 
 import com.rookies4.myspringboot.entity.UserEntity;
+import com.rookies4.myspringboot.exception.BusinessException;
 import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor // final이 붙은 변수의 생성자를 초기화해줌
@@ -19,14 +22,28 @@ public class UserRestController {
 //        this.userRepository = userRepository;
 //    }
 
-    @PostMapping //등록
+    //등록
+    @PostMapping
     public UserEntity create(@RequestBody UserEntity user) {
         return userRepository.save(user);
     }
 
+    //전체목록 조회
     @GetMapping
     public List<UserEntity> getUsers() {
         return userRepository.findAll();
+    }
+
+    //개별 조회 : ID로
+    @GetMapping("/{id}")
+    public UserEntity getUser(@PathVariable Long id) {
+        Optional<UserEntity> optionalUser = userRepository.findById(id);
+        //orElseThrow(Supplier) Supplier의 추상메서드 T get()
+        UserEntity existUser = optionalUser
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+        return existUser;
+
+
     }
 
 }
