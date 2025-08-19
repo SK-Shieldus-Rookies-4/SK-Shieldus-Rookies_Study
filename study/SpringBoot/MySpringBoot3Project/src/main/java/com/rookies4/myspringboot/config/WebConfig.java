bdc16dda630cd5fe9ded5e09ab351071815c95e3
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-08-19 09:58:06 +0900

    addCorsMappings

--- study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/config/WebConfig.java
+++ study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("*");;
+                .allowedMethods("*");
 
     }
 }
