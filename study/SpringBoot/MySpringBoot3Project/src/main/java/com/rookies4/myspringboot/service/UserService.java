9d68ff17b93f9295ebd9c6cf7125586745d3d03f
JIEUN <163732805+jxieun@users.noreply.github.com>
2025-08-20 10:55:16 +0900

    UserService update

--- study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/service/UserService.java
+++ study/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/service/UserService.java
@@ -1,11 +1,31 @@
 package com.rookies4.myspringboot.service;
 
+import com.rookies4.myspringboot.controller.dto.UserDTO;
+import com.rookies4.myspringboot.entity.UserEntity;
+import com.rookies4.myspringboot.exception.BusinessException;
 import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
     private final UserRepository userRepository;
+
+    //User 등록
+    @Transactional
+    public UserDTO.UserResponse createUser(UserDTO.UserCreateRequest request) {
+        //Email 중복검사
+        userRepository.findByEmail(request.getEmail()) //Optional<UserEntity>
+                .ifPresent(entity -> {
+                    throw new BusinessException("User with this Email already Exist", HttpStatus.CONFLICT);
+                });
+        //DTO => Entity 로 변환
+        UserEntity entity = request.toEntity();
+        UserEntity savedEntity = userRepository.save(entity);
+        return new UserDTO.UserResponse(savedEntity)
+    }
 }
